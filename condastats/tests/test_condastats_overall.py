93e3e57cd9262c45790d46890889494e582fc251
Sophia Yang <sophimyang@gmail.com>
2019-11-15 16:22:54 -0600

    output is pandas series now, we need to use loc to find the actual number

--- condastats/tests/test_condastats_overall.py
+++ condastats/tests/test_condastats_overall.py
@@ -1,11 +1,12 @@
 from condastats.cli import load_pkg_month, pkg_platform_month, pkg_python_month, pkg_version_month, data_source_month
 
 def test_load_pkg_month1():
-    x = load_pkg_month('pandas', month='2019-01')
-    assert x == 932443
+    x = load_pkg_month(['pandas','dask'], month='2019-01')
+    assert x.loc['pandas'] == 932443
+    assert x.loc['dask'] == 221200
 
 def test_load_pkg_month2():
     x = load_pkg_month('pandas',month='2019-01', pkg_platform='linux-32',data_source='anaconda',pkg_version='0.10.0',pkg_python=2.6)
-    assert x == 12
+    assert x.loc['pandas'] == 12
 
 
