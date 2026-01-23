b1f794ef0f184b8ed520374e380d5cedf18004ba
Jannis Leidel <jannis@leidel.info>
2026-01-23 19:29:52 +0100

    Drop Python 3.8 and 3.9 support, require Python 3.10+
    Update package classifiers to support Python 3.10-3.13 and add
    python_requires constraint to enforce minimum version.

--- setup.py
+++ setup.py
@@ -34,11 +34,12 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
     ],
+    python_requires='>=3.10',
     description="Conda package stats CLI",
     entry_points={
         'console_scripts': [
