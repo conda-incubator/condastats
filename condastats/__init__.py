dd350847e740729af919ddb3a04a05956c6e8cf5
Jannis Leidel <jannis@leidel.info>
2026-01-23 19:39:38 +0100

    Migrate to pyproject.toml with setuptools_scm
    Replace legacy setup.py/versioneer.py with modern pyproject.toml
    using setuptools_scm for version management.
    
    - Add pyproject.toml with all project metadata and dependencies
    - Update __init__.py to use importlib.metadata for version
    - Remove setup.py, setup.cfg, versioneer.py, _version.py
    - Remove .gitattributes (versioneer export-subst)
    - Remove MANIFEST.in (not needed with modern setuptools)
    - Remove unused requirements.txt

--- condastats/__init__.py
+++ condastats/__init__.py
@@ -2,9 +2,11 @@
 
 """Top-level package for condastats."""
 
-__author__ = """Sophia Man Yang"""
-__version__ = '0.1.2'
+from importlib.metadata import version, PackageNotFoundError
 
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+__author__ = """Sophia Man Yang"""
+
+try:
+    __version__ = version("condastats")
+except PackageNotFoundError:
+    __version__ = "0.0.0"
